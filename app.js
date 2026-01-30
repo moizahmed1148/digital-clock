da44200faa3f562cbcc38e4d85dbe909f6f005f4
moizahmed1148 <moiza1jj2@gmail.com>
2026-01-30 08:09:37 +0500

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 var time = document.getElementById("time");
 var dates = document.getElementById("date");
+var clock = document.getElementById("clock");
 function updateClock() {
     var now = new Date();
 
@@ -8,17 +23,27 @@ function updateClock() {
     var m = now.getMinutes();
     var s = now.getSeconds();
     console.log(m);
-    
-   if(m < 10){
-    time.innerText = + "0" + h + " : " + "0" + m + " : " + "0"  + s;
+    if(h === 0){
+        clock.innerText = "AM"
     }
-    else{
-        time.innerText = h + " : " +  m + " : " + s;
+    if(h > 12){
+        h = h - 12
+        clock.innerText = "PM"
     }
 
+    if(h < 10){
+       h = "0" + h;
+    }
+    if(m < 10){
+      m = "0"  + m ;
+    }
+    if(s < 10){
+      s =  "0" + s;
+    }
+    time.innerText =  h + " : " + m + " : "+ s;
     var day = days[now.getDay()];
     var date = now.getDate();
 
-    dates.innerText = day + " : " + date + " "
+    dates.innerText = day + " : " + date + " " + " " + months[now.getMonth()] + " " + now.getFullYear()
 }
 setInterval(updateClock,1000);
